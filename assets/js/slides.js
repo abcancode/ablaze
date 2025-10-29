8a09e715860fc563aa45ed1a07bc93a27031d958
abcancode <chidozieintech@gmail.com>
2025-10-29 21:58:32 +0100

    update changes

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,32 +1,3 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const mobileMenu = document.querySelector(".mobile-menu");
-const backToTopBtn = document.getElementById("backToTop");
-
-menuToggle.addEventListener("click", () => {
-  menuToggle.classList.toggle("active");
-  mobileMenu.classList.toggle("active");
-});
-
-// Show or hide button on scroll
-window.onscroll = function () {
-  if (
-    document.body.scrollTop > 200 ||
-    document.documentElement.scrollTop > 200
-  ) {
-    backToTopBtn.style.display = "block";
-  } else {
-    backToTopBtn.style.display = "none";
-  }
-};
-
-// Smooth scroll to top on click
-backToTopBtn.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
-
 const slides = document.querySelectorAll(".video-slide");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
